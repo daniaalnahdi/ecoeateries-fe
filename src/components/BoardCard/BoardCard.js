5c732107a4be3574351afbffb580415dae51eae3
daniaalnahdi <alnahdi.d@husky.neu.edu>
2020-08-22 01:25:20 +0300

    add drag and drop

--- src/components/BoardCard/BoardCard.js
+++ src/components/BoardCard/BoardCard.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
-const BoardCard = ({ goal, category }) => {
+import './BoardCard.scss';
+
+const BoardCard = ({ goal, category, id }) => {
+  const dragStart = (e) => {
+    const target = e.target;
+    e.dataTransfer.setData('card_id', target.id);
+
+    setTimeout(() => {
+      target.style.display = 'none';
+    }, 0);
+  };
+
+  const dragOver = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className='box'>
+    <div
+      className='box board-card'
+      id={id}
+      onDragStart={dragStart}
+      onDragOver={dragOver}
+      draggable='true'
+    >
       <p>Category: {category}</p>
       <p>Goal: {goal}</p>
     </div>
--- src/components/BoardColumn/BoardColumn.js
+++ src/components/BoardColumn/BoardColumn.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
-const BoardColumn = ({ title, children }) => {
+import './BoardColumn.scss';
+
+const BoardColumn = ({ title, children, updateGoalStatus, id }) => {
+  const drop = (e) => {
+    e.preventDefault();
+    const cardId = e.dataTransfer.getData('card_id');
+
+    const card = document.getElementById(cardId);
+    card.style.display = 'block';
+
+    e.target.appendChild(card);
+    updateGoalStatus(cardId);
+  };
+
+  const dragOver = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className='column'>
-      <div className='box'>
+    <>
+      <div
+        className='column board-column box'
+        id={id}
+        onDrop={drop}
+        onDragOver={dragOver}
+      >
         <h3 className='title is-4'>{title}</h3>
+
         {children}
       </div>
-    </div>
+    </>
   );
 };
 
